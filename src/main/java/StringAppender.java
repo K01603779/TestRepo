d7961663b71658cb7185b9a575ed35491344c85d
K01603779 <73077750+K01603779@users.noreply.github.com>
2026-07-15 11:42:10 +0200

    apply spotless

--- src/main/java/StringAppender.java
+++ src/main/java/StringAppender.java
@@ -1,6 +1,6 @@
 public class StringAppender {
 
-    public static String append(String a, String b) {
-        return a + b;
-    }
+  public static String append(String a, String b) {
+    return a + b;
+  }
 }
