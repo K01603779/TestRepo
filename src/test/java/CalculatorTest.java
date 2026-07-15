b389831498822d2f9a8cb1211dbad55a33871dfb
K01603779 <73077750+K01603779@users.noreply.github.com>
2026-07-15 08:24:32 +0200

    update unit-tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,12 +1,13 @@
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class CalculatorTest {
 
     @Test
     void addNumbers(){
-        assertTrue(false);
+        assertEquals(10, Calculator.add(5, 5));
     }
 }
