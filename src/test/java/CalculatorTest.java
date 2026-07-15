2188d97262584efabc0fb047bb5f9f646dbb1c26
Marcel <73077750+K01603779@users.noreply.github.com>
2026-07-15 08:51:27 +0200

    K01603779 GitHub actions (#3)

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,13 +1,11 @@
+import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 public class CalculatorTest {
 
-    @Test
-    void addNumbers(){
-        assertEquals(10, Calculator.add(5, 5));
-    }
+  @Test
+  void addNumbers() {
+    assertEquals(10, Calculator.add(5, 5));
+  }
 }
