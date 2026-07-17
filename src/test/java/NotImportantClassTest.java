7d487db627205e4ad32109ff1778c59010adfd36
Andrei Blokhin <andrei_blokhin@outlook.com>
2026-07-17 07:49:32 +0200

    feat: added features (#28)
    Co-authored-by: Marcel <73077750+K01603779@users.noreply.github.com>

---
+++ src/test/java/NotImportantClassTest.java
@@ -0,0 +1,40 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class NotImportantClassTest {
+  private IDoSomething doSomething;
+
+  @BeforeEach
+  public void beforeEach() {
+    doSomething = new NotImportantClass();
+  }
+
+  @Test
+  public void testDoSomething() {
+    Assertions.assertEquals("Doing something...", doSomething.doSomething());
+  }
+
+  @Test
+  public void testFailingDoSomething() {
+    // Assertions.assertEquals("", doSomething.doSomething());
+  }
+
+  @Test
+  public void longStream() {
+    var list = List.of(1, 52, 3, 4, 66, 868, 32, 234, -1);
+
+    list.stream()
+        .sorted()
+        .filter(x -> x < 100)
+        .collect(Collectors.toSet())
+        .forEach(x -> x.intValue());
+  }
+
+  // should be spotted by spotbugs?
+  public boolean equals(Object obj) {
+    return true;
+  }
+}
